32ae2d2c48bc8968d396152e3432f6a502d3dbd0
1Omon <1serwise@gmail.com>
2024-10-11 13:03:12 +0000

    haha, this will be quite fun

---
+++ dist/_db.js
@@ -0,0 +1,77 @@
+let games = [
+    {
+        id: '1',
+        title: 'The Witcher 3',
+        platform: ['PS4', 'Xbox One']
+    },
+    {
+        id: '2',
+        title: 'The Last of Us',
+        platform: ['PS4', 'PS5']
+    },
+    {
+        id: '3',
+        title: 'The Last of Us Part II',
+        platform: ['PS4', 'PS5']
+    },
+    {
+        id: '4',
+        title: 'God of War',
+        platform: ['PS4', 'PC']
+    },
+    {
+        id: '5',
+        title: 'Horizon Zero Dawn',
+        platform: ['PS4', 'PC']
+    },
+];
+let reviews = [
+    {
+        id: '1',
+        rating: 5,
+        content: 'This game is amazing!'
+    },
+    {
+        id: '2',
+        rating: 4,
+        content: 'This game is good!'
+    },
+    {
+        id: '3',
+        rating: 3,
+        content: 'This game is ok!'
+    },
+    {
+        id: '4',
+        rating: 2,
+        content: 'This game is bad!'
+    },
+    {
+        id: '5',
+        rating: 1,
+        content: 'This game is terrible!'
+    },
+    {
+        id: '6',
+        rating: 5,
+        content: 'This game is amazing!'
+    }
+];
+let authors = [
+    {
+        id: '1',
+        name: 'John Doe',
+        verified: true
+    },
+    {
+        id: '2',
+        name: 'Jane Doe',
+        verified: false
+    },
+    {
+        id: '3',
+        name: 'Jim Doe',
+        verified: true
+    }
+];
+export default { games, reviews, authors };
